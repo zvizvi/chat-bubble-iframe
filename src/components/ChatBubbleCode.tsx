96e2a57e4564dc58a164357f32c97e632ea6b762
zvizvi <5633910@gmail.com>
2025-03-12 20:17:40 +0200

    Refactor: Update SVG icons in ChatBubbleCode component

--- src/components/ChatBubbleCode.tsx
+++ src/components/ChatBubbleCode.tsx
@@ -25,11 +25,11 @@ const ChatBubbleCode = () => {
     // 2. Or a full SVG string as shown below
     buttonIcon: 'message-circle', // Icon when chat is closed (using keyword)
     // Alternative with SVG:
-    // buttonIcon: '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 12C21 16.9706 16.9706 21 12 21C10.8976 21 9.82578 20.8204 8.82353 20.4857C8.74178 20.4594 8.65567 20.4472 8.57143 20.4472C8.4881 20.4472 8.40477 20.4594 8.32353 20.4857L3.58333 21.9857C3.22241 22.093 2.83138 21.8562 2.83333 21.4764V16.4671C2.83333 16.3682 2.80277 16.2713 2.74609 16.1868C1.65389 14.5489 1 12.576 1 10.5C1 5.52944 5.02944 1.5 10 1.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+    // buttonIcon: '<svg viewBox="0 0 24 24" fill="none"><path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
 
     openButtonIcon: 'close', // Icon when chat is open (using keyword)
     // Alternative with SVG:
-    // openButtonIcon: '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 6L6 18M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+    // openButtonIcon: '<svg viewBox="0 0 24 24" fill="none"><path d="M18 6L6 18M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
 
     hideHeader: false, // Set to true to completely hide the header
     headerBackground: '#f9fafb', // Header background color
